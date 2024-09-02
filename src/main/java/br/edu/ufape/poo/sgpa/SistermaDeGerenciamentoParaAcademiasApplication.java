c14f3ebed19640b92d9df81ca4e0f06cbdcec9a4
Genildo Burgos <gnldburgos@gmail.com>
2024-09-02 17:47:54 -0300

    First Commit

---
+++ src/main/java/br/edu/ufape/poo/sgpa/SistermaDeGerenciamentoParaAcademiasApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufape.poo.sgpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistermaDeGerenciamentoParaAcademiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistermaDeGerenciamentoParaAcademiasApplication.class, args);
+	}
+
+}
