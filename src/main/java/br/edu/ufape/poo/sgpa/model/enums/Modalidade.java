a0a5363ca3cb95c764fe528245fa42846caed91c
Hassiria Yasmim <hassiriayasmims@gmail.com>
2024-09-03 15:28:43 -0300

    Apenas Enum modalidade

---
+++ src/main/java/br/edu/ufape/poo/sgpa/model/enums/Modalidade.java
@@ -0,0 +1,18 @@
+package br.edu.ufape.poo.sgpa.model.enums;
+
+public enum Modalidade {
+    MUSCULACAO,
+    FUNCIONAL,
+    MUAYTHAI,
+    PILATES,
+    ZUMBA,
+    CROSSFIT,
+    BOXE,
+    TAEKWONDO,
+    CAPOEIRA,
+    JIUJITSU,
+    BALE,
+    YOGA,
+    NATACAO,
+    OUTROS;
+}
