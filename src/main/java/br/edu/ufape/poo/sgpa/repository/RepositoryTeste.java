54325c1615632e1d54bb17688d373949f984ae06
Genildo Burgos <gnldburgos@gmail.com>
2024-09-04 00:07:19 -0300

    Correção repository e nome da class main

--- src/main/java/br/edu/ufape/poo/sgpa/repository/RepositoryTeste.java
+++ src/main/java/br/edu/ufape/poo/sgpa/repository/RepositoryTeste.java
@@ -1,6 +1,10 @@
 package br.edu.ufape.poo.sgpa.repository;
 
+import br.edu.ufape.poo.sgpa.model.modeTest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface RepositoryTeste extends JpaRepository {
+@Repository
+public interface RepositoryTeste extends JpaRepository<modeTest, Long> {
+
 }
