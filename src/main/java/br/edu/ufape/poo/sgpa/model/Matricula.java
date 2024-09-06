78b558072e4e1d142710de02fb55924914aaf236
Analice Silva <analiceds.nascimento@gmail.com>
2024-09-06 13:54:18 -0300

    Adicionar as relações das classes matrícula e sala com o banco de dados

--- src/main/java/br/edu/ufape/poo/sgpa/model/Matricula.java
+++ src/main/java/br/edu/ufape/poo/sgpa/model/Matricula.java
@@ -1,18 +1,50 @@
 package br.edu.ufape.poo.sgpa.model;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 import br.edu.ufape.poo.sgpa.model.enums.StatusDaMatricula;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "matricula", schema = "academia")
 public class Matricula {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+	@Column(name="data_de_matricula")
 	private LocalDate dataDeMatricula;
+	
+	@Column(name="data_de_expiracao")
 	private LocalDate dataDeExpiracao;
+	
+	@Enumerated(EnumType.STRING)
 	private StatusDaMatricula statusDaMatricula;
+	
+	//@ManyToOne
+    //private Membro membro;
+	
+	//@ManyToOne
+	//private Turma turma; 
+	
+	public Matricula() {	
+	}
 	
 	public Matricula(LocalDate dataDeMatricula, LocalDate dataDeExpiracao) {
 		this.dataDeMatricula = dataDeMatricula;
 		this.dataDeExpiracao = dataDeExpiracao;
 		this.statusDaMatricula = StatusDaMatricula.ATIVA;
+		//this.membro = membro;
+		//this.turma = turma;
 	}
 	
 	public void ativarMatricula() {
@@ -21,6 +53,43 @@ public class Matricula {
 	
 	public void suspenderMatricula() {
 		this.statusDaMatricula = StatusDaMatricula.SUSPENSA;
+	}
+	
+	public boolean estaExpirada() {
+        return LocalDate.now().isAfter(this.dataDeExpiracao);
+    }
+	
+	@Override
+    public String toString() {
+        return "Matricula{" +
+                "id=" + id +
+                ", dataDeMatricula=" + dataDeMatricula +
+                ", dataDeExpiracao=" + dataDeExpiracao +
+                ", statusDaMatricula=" + statusDaMatricula +
+               // ", membro=" + membro + membro.toString()
+               // ", turma=" + turma + turma.toString()
+                '}';
+    }
+
+    /*@Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Matricula matricula)) return false;
+        return getId() == matricula.getId() &&
+               Objects.equals(getDataDeMatricula(), matricula.getDataDeMatricula()) &&
+               Objects.equals(getDataDeExpiracao(), matricula.getDataDeExpiracao()) &&
+               getStatusDaMatricula() == matricula.getStatusDaMatricula() &&
+               Objects.equals(getMembro(), matricula.getMembro()) &&
+               Objects.equals(getTurma(), matricula.getTurma());
+    }/*
+
+    /*@Override
+    public int hashCode() {
+        return Objects.hash(getId(), getDataDeMatricula(), getDataDeExpiracao(), getStatusDaMatricula(), getMembro(), getTurma());
+    }*/
+
+	public long getId() {
+		return id;
 	}
 
 	public LocalDate getDataDeMatricula() {
@@ -47,4 +116,19 @@ public class Matricula {
 		this.statusDaMatricula = statusDaMatricula;
 	}
 
+	/*public Membro getMembro() {
+		return membro;
+	}
+
+	public void setMembro(Membro membro) {
+		this.membro = membro;
+	}
+
+	public Turma getTurma() {
+		return turma;
+	}
+
+	public void setTurma(Turma turma) {
+		this.turma = turma;
+	}*/
 }
--- src/main/java/br/edu/ufape/poo/sgpa/model/Sala.java
+++ src/main/java/br/edu/ufape/poo/sgpa/model/Sala.java
@@ -1,16 +1,71 @@
 package br.edu.ufape.poo.sgpa.model;
 
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table ( name = "sala", schema = "academia")
 public class Sala {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+	
+	@Column(name="bloco")
 	private String bloco;
+	
+	@Column(name="numero")
 	private String numero;
+	
+	@Column(name="capacidade")
 	private int capacidade;
-	// Falta classe slotHorario
+	
+	//@OneToMany(mappedBy = "sala")
+	//private List<SlotHorario> slotsHorario;
+	
+	public Sala() {
+	}
 	
 	public Sala(String bloco, String numero, int capacidade) {
 		this.bloco = bloco;
 		this.numero = numero;
 		this.capacidade = capacidade;
-		// Falta classe slotHorario
+		//this.slotsHorario = new ArrayList<>();
+	}
+	
+	@Override
+    public String toString() {
+        return "Sala{" +
+                "id=" + id +
+                ", bloco='" + bloco + '\'' +
+                ", numero='" + numero + '\'' +
+                ", capacidade=" + capacidade +
+                //", slotsHorario=" + slotsHorario.toString()
+                '}';
+    }
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Sala sala)) return false;
+			return getId() == sala.getId() &&
+	               getCapacidade() == sala.getCapacidade() &&
+	               Objects.equals(getBloco(), sala.getBloco()) &&
+	               Objects.equals(getNumero(), sala.getNumero());
+	}
+	
+	@Override
+    public int hashCode() {
+        return Objects.hash(getId(), getBloco(), getNumero(), getCapacidade());
+    }
+	 
+	public long getId() {
+		return id;
 	}
 
 	public String getBloco() {
@@ -38,6 +93,6 @@ public class Sala {
 	}
 	
 	public void reservarSala() {
-		// Falta classe SlotHorario
+		//this.slotsHorario.add(slotHorario);
 	}
 }
