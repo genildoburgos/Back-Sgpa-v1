077a36206d232c1fb092a7323a90e8b8b88d5cd2
Analice Silva <analiceds.nascimento@gmail.com>
2024-09-09 14:58:49 -0300

    Adicionar relção com slotHorario e Sala

--- src/main/java/br/edu/ufape/poo/sgpa/model/Matricula.java
+++ src/main/java/br/edu/ufape/poo/sgpa/model/Matricula.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
@@ -35,6 +36,7 @@ public class Matricula {
     private Membro membro;
 	
 	@ManyToOne
+	@JoinColumn(name = "turma_id")
 	private Turma turma; 
 	
 	public Matricula() {	
