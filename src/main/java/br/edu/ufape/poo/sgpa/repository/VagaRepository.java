7f71ab4182d5f4b5d093072ec3a211598a816170
Genildo Burgos <gnldburgos@gmail.com>
2024-09-10 00:45:59 -0300

    Criação de vaga repository

---
+++ src/main/java/br/edu/ufape/poo/sgpa/repository/VagaRepository.java
@@ -0,0 +1,8 @@
+package br.edu.ufape.poo.sgpa.repository;
+
+import br.edu.ufape.poo.sgpa.model.Vaga;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VagaRepository extends JpaRepository<Vaga, Long> {
+
+}
