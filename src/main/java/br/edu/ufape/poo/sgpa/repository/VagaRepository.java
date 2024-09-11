43aa99b19c61ba54dc926b2e5e0daed3f74a6b99
Genildo Burgos <gnldburgos@gmail.com>
2024-09-11 10:38:45 -0300

    Correção do nome da classe vaga e remoção de repositorio de responsavel

--- src/main/java/br/edu/ufape/poo/sgpa/repository/VagaRepository.java
+++ src/main/java/br/edu/ufape/poo/sgpa/repository/VagaRepository.java
@@ -1,8 +1,8 @@
 package br.edu.ufape.poo.sgpa.repository;
 
-import br.edu.ufape.poo.sgpa.model.Vagas;
+import br.edu.ufape.poo.sgpa.model.Vaga;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VagaRepository extends JpaRepository<Vagas, Long> {
+public interface VagaRepository extends JpaRepository<Vaga, Long> {
 
 }
