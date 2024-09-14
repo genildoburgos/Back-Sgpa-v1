c1f818e95d5a3c187ea581eb93ec1f99a265ca6b
Ravenna-Santos <ravennasantos2016@gmail.com>
2024-09-13 23:35:15 -0300

    cria repositorio de matriculas

---
+++ src/main/java/br/edu/ufape/poo/sgpa/repository/MatriculaRepository.java
@@ -0,0 +1,22 @@
+package br.edu.ufape.poo.sgpa.repository;
+
+import br.edu.ufape.poo.sgpa.model.Matricula;
+import br.edu.ufape.poo.sgpa.model.Membro;
+import br.edu.ufape.poo.sgpa.model.Turma;
+import br.edu.ufape.poo.sgpa.model.enums.StatusDaMatricula;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface MatriculaRepository extends JpaRepository<Matricula, Long> {
+
+    Optional<Matricula> findByMembro(Membro membro);
+    List<Matricula> findByDataDeMatricula(LocalDate dataDeMatricula);
+    List<Matricula> findBydataDeExpiracao(LocalDate dataDeExpiracao);
+    List<Matricula> findByStatusDaMatricula(StatusDaMatricula status);
+    List<Matricula> findByTurma(Turma turma);
+
+
+}
