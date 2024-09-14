01740335c588f3e7c4f8687c8b4bc9e41cdd84ee
Hassiria Yasmim <hassiriayasmims@gmail.com>
2024-09-14 17:33:37 -0300

    Adicionado facade adm

--- src/main/java/br/edu/ufape/poo/sgpa/facade/Facade.java
+++ src/main/java/br/edu/ufape/poo/sgpa/facade/Facade.java
@@ -3,9 +3,11 @@ package br.edu.ufape.poo.sgpa.facade;
 import br.edu.ufape.poo.sgpa.model.Membro;
 import br.edu.ufape.poo.sgpa.model.Plano;
 import br.edu.ufape.poo.sgpa.model.Vaga;
+import br.edu.ufape.poo.sgpa.model.Administrador;
 import br.edu.ufape.poo.sgpa.service.MembroService;
 import br.edu.ufape.poo.sgpa.service.PlanoService;
 import br.edu.ufape.poo.sgpa.service.VagaService;
+import br.edu.ufape.poo.sgpa.service.AdministradorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,10 +16,11 @@ import java.util.List;
 @Service
 public class Facade {
 
-    public Facade(MembroService membroService, PlanoService planoService, VagaService vagaService) {
+    public Facade(MembroService membroService, PlanoService planoService, VagaService vagaService, AdministradorService administradorService) {
 		this.membroService = membroService;
         this.planoService = planoService;
         this.vagaService = vagaService;
+        this.administradorService = administradorService;
     }
     
     // ------------------------ Membro --------------------------------
@@ -67,5 +70,37 @@ public class Facade {
     // Atulizar vaga
     public Vaga atualizarVaga(Vaga obj, Long id) {return vagaService.atualizarVaga(obj, id);}
 
+     // ------------------------ Administrador --------------------------------
 
-}
+     @Autowired
+     private final AdministradorService administradorService;
+
+     public List<Administrador> listarAdministradores(){
+        return administradorService.listarAdministradores();
+     }
+
+     public Administrador criarAdministrador(Administrador novaInstancia){
+        return administradorService.criarAdministrador(novaInstancia);
+     }
+
+     public void deletarAdministrador(Long id){
+        administradorService.deletarAdministrador(id);
+     }
+
+     public Administrador buscarAdministradorPorId(Long id){
+        return administradorService.buscarAdministradorPorId(id);
+     }
+
+     public Administrador atualizarAdministrador(Administrador administrador, Long id){
+        return administradorService.atualizarAdministrador(administrador, id);
+     }
+
+     public Administrador buscAdministradorPorCpf(String cpf){
+        return administradorService.buscarAdministradorPorCpf(cpf);
+     }
+
+     public List<Administrador> buscarAdministradoresPorNome(String nome){
+        return administradorService.buscarAdministradoresPorNome(nome);
+     }
+
+    }
