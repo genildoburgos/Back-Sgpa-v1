009239c3aa3b7213bd20d2e42508f78ff961a356
Ravenna-Santos <ravennasantos2016@gmail.com>
2024-09-14 20:40:43 -0300

    cria service matricula

--- src/main/java/br/edu/ufape/poo/sgpa/model/IMatricula.java
+++ src/main/java/br/edu/ufape/poo/sgpa/model/IMatricula.java
@@ -1,9 +1,11 @@
 package br.edu.ufape.poo.sgpa.model;
 
 public interface IMatricula {
+    //Apagar interface matricula?
+    /*
     void ativarMatricula();
 
     void suspenderMatricula();
 
-    boolean estaExpirada();
+    boolean estaExpirada(); */
 }
