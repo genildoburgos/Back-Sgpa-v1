318a4b96199596fa5d9e0045de2de113b9ca26f7
Hassiria Yasmim <hassiriayasmims@gmail.com>
2024-09-15 17:28:22 -0300

    Adicionado dto de plano

---
+++ src/main/java/br/edu/ufape/poo/sgpa/controller/dto/response/PlanoResponse.java
@@ -0,0 +1,53 @@
+package br.edu.ufape.poo.sgpa.controller.dto.response;
+
+import br.edu.ufape.poo.sgpa.model.Plano;
+import br.edu.ufape.poo.sgpa.model.Unidade;
+import br.edu.ufape.poo.sgpa.model.enums.Periodicidade;
+
+public class PlanoResponse {
+    
+    private double valor;
+    private String dataDeVencimento;
+    private Periodicidade periodicidade;
+    private Unidade unidade;
+
+    public PlanoResponse(){
+
+    }
+
+    public PlanoResponse(Plano plano) {
+        this.valor = plano.getValor();
+        this.dataDeVencimento = plano.getDataDeVencimento();
+        this.periodicidade = plano.getPeriodicidade();
+        this.unidade = plano.getUnidade();
+    }
+
+    public double getValor(){
+        return valor;
+    }
+    public void setValor(double valor){
+        this.valor = valor;
+    }
+
+    public String getDataDeVencimento(){
+        return dataDeVencimento;
+    }
+    public void setDataDeVencimento(String dataDeVencimento){
+        this.dataDeVencimento = dataDeVencimento;
+    }
+
+    public Periodicidade getPeriodicidade(){
+        return periodicidade;
+    }
+    public void setDataDeVencimento(Periodicidade periodicidade){
+        this.periodicidade = periodicidade;
+    }
+
+    public Unidade getUnidade(){
+        return unidade;
+    }
+    public void setUnidade(Unidade unidade){
+        this.unidade = unidade;
+    }
+
+}
