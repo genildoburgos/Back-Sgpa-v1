76b48416b4a1ba189909993345c8066729f4b23f
Ravenna-Santos <ravennasantos2016@gmail.com>
2024-09-14 22:19:16 -0300

    atualiza fachada adicionando unidade e instrutor

--- src/main/java/br/edu/ufape/poo/sgpa/facade/Facade.java
+++ src/main/java/br/edu/ufape/poo/sgpa/facade/Facade.java
@@ -1,11 +1,7 @@
 package br.edu.ufape.poo.sgpa.facade;
 
-import br.edu.ufape.poo.sgpa.model.Membro;
-import br.edu.ufape.poo.sgpa.model.Plano;
-import br.edu.ufape.poo.sgpa.model.Vaga;
-import br.edu.ufape.poo.sgpa.service.MembroService;
-import br.edu.ufape.poo.sgpa.service.PlanoService;
-import br.edu.ufape.poo.sgpa.service.VagaService;
+import br.edu.ufape.poo.sgpa.model.*;
+import br.edu.ufape.poo.sgpa.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,10 +10,12 @@ import java.util.List;
 @Service
 public class Facade {
 
-    public Facade(MembroService membroService, PlanoService planoService, VagaService vagaService) {
+    public Facade(MembroService membroService, PlanoService planoService, VagaService vagaService, InstrutorService instrutorService, UnidadeService unidadeService) {
 		this.membroService = membroService;
         this.planoService = planoService;
         this.vagaService = vagaService;
+        this.instrutorService = instrutorService;
+        this.unidadeService = unidadeService;
     }
     
     // ------------------------ Membro --------------------------------
@@ -67,5 +65,44 @@ public class Facade {
     // Atulizar vaga
     public Vaga atualizarVaga(Vaga obj, Long id) {return vagaService.atualizarVaga(obj, id);}
 
+
+
+    //---------------------- Instrutor ---------------------------
+    @Autowired
+    private final InstrutorService instrutorService;
+
+    //Listar instrutor
+    public List <Instrutor> listarInstrutores() {return instrutorService.listarInstrutores();}
+
+    //criar instrutor
+    public Instrutor criarInstrutor(Instrutor novoInstrutor) {return instrutorService.criarInstrutor(novoInstrutor);}
+
+    // deletar instrutor
+    public void deletarInstrutor(Long id){instrutorService.deletarIntrutor(id);}
+
+    //buscar instrutor por id
+    public Instrutor buscarInstrutor(Long id){return instrutorService.buscarInstrutor(id);}
+
+    //atualizar instrutor
+    public Instrutor atualizarInstrutor(Instrutor instrutor, Long id){return instrutorService.atualizarInsturor(instrutor, id);}
+
+
+    //------------------- Unidade --------------------
+    @Autowired
+    private final UnidadeService unidadeService;
+    //listar unidades
+    public List<Unidade> listarUnidades() {return unidadeService.listarUnidades();}
+
+    //criar unidade
+    public Unidade criarUnidade(Unidade unidade) {return unidadeService.criarUnidade(unidade);}
+
+    //deletar unidade
+    public void deletarUnidade(Long id){unidadeService.deletarUnidade(id);}
+
+    //buscar unidade por id
+    public Unidade buscarUnidade(Long id){return unidadeService.buscarUnidade(id);}
+
+    //atualizar unidade
+    public Unidade atualizarUnidade(Unidade unidade, Long id){return unidadeService.atualizarUnidade(unidade, id);}
 
 }
