0ca42865de63ae18f10b430e959047af845abc9b
Ravenna-Santos <ravennasantos2016@gmail.com>
2024-09-17 00:28:48 -0300

    cria dto instrutor

---
+++ src/main/java/br/edu/ufape/poo/sgpa/controller/dto/request/InstrutorRequest.java
@@ -0,0 +1,63 @@
+package br.edu.ufape.poo.sgpa.controller.dto.request;
+
+import br.edu.ufape.poo.sgpa.model.Instrutor;
+import br.edu.ufape.poo.sgpa.model.SlotHorario;
+import br.edu.ufape.poo.sgpa.model.Unidade;
+import br.edu.ufape.poo.sgpa.model.enums.Modalidade;
+
+import java.time.LocalDate;
+import java.util.List;
+
+// TODO: Criar DTO para Pessoa e colocar herança
+public class InstrutorRequest {
+    private String clt;
+    private List<SlotHorario> horariosDeTrabalho;
+    private List<Modalidade> modalidades;
+    private List<Unidade> unidades;
+
+    public InstrutorRequest() {
+    }
+
+    public String getClt() {
+        return clt;
+    }
+
+    public void setClt(String clt) {
+        this.clt = clt;
+    }
+
+    public List<SlotHorario> getHorariosDeTrabalho() {
+        return horariosDeTrabalho;
+    }
+
+    public void setHorariosDeTrabalho(List<SlotHorario> horariosDeTrabalho) {
+        this.horariosDeTrabalho = horariosDeTrabalho;
+    }
+
+    public List<Modalidade> getModalidades() {
+        return modalidades;
+    }
+
+    public void setModalidades(List<Modalidade> modalidades) {
+        this.modalidades = modalidades;
+    }
+
+    public List<Unidade> getUnidades() {
+        return unidades;
+    }
+
+    public void setUnidades(List<Unidade> unidades) {
+        this.unidades = unidades;
+    }
+
+    public Instrutor toInstrutor(){
+        Instrutor instrutor = new Instrutor();
+
+        instrutor.setClt(this.clt);
+        instrutor.setHorariosDeTrabalho(this.horariosDeTrabalho);
+        instrutor.setModalidades(this.modalidades);
+        instrutor.setUnidades(this.unidades);
+
+        return instrutor;
+    }
+}
---
+++ src/main/java/br/edu/ufape/poo/sgpa/controller/dto/response/InstrutorResponse.java
@@ -0,0 +1,66 @@
+package br.edu.ufape.poo.sgpa.controller.dto.response;
+
+// TODO: Adicionar herença da classe pessoa
+
+import br.edu.ufape.poo.sgpa.model.Instrutor;
+import br.edu.ufape.poo.sgpa.model.SlotHorario;
+import br.edu.ufape.poo.sgpa.model.Unidade;
+import br.edu.ufape.poo.sgpa.model.enums.Modalidade;
+
+import java.util.List;
+
+public class InstrutorResponse {
+    private Long id;
+    private String clt;
+    private List<SlotHorario> horariosDeTrabalho;
+    private List<Modalidade> modalidades;
+    private List<Unidade> unidades;
+
+    public InstrutorResponse(Instrutor instrutor) {
+        this.id = instrutor.getId();
+        this.clt = instrutor.getClt();
+        this.horariosDeTrabalho = instrutor.getHorariosDeTrabalho();
+        this.modalidades = instrutor.getModalidades();
+        this.unidades = instrutor.getUnidades();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getClt() {
+        return clt;
+    }
+
+    public void setClt(String clt) {
+        this.clt = clt;
+    }
+
+    public List<SlotHorario> getHorariosDeTrabalho() {
+        return horariosDeTrabalho;
+    }
+
+    public void setHorariosDeTrabalho(List<SlotHorario> horariosDeTrabalho) {
+        this.horariosDeTrabalho = horariosDeTrabalho;
+    }
+
+    public List<Modalidade> getModalidades() {
+        return modalidades;
+    }
+
+    public void setModalidades(List<Modalidade> modalidades) {
+        this.modalidades = modalidades;
+    }
+
+    public List<Unidade> getUnidades() {
+        return unidades;
+    }
+
+    public void setUnidades(List<Unidade> unidades) {
+        this.unidades = unidades;
+    }
+}
